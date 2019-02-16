d989bf0736b6648ea24faf13ee8f40216692bb3f
JerYOMat <jeremy.emata@gmail.com>
2019-02-15 19:57:37 -0500

    update routes for static pages controller and set root

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  get 'static_pages/home'
-  get 'static_pages/code_of_conduct'
-  get 'static_pages/credits'
+  root 'static_pages#home'
+  get 'static_pages/code_of_conduct', to: 'static_pages#code_of_conduct'
+  get 'static_pages/credits', to: 'static_pages#credits'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
