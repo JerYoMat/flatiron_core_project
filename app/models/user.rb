a30f181f01f75478033c08a539d47ce44c3a0ef8
JerYOMat <jeremy.emata@gmail.com>
2019-02-16 06:26:39 -0500

    added basic user tests

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ApplicationRecord
   has_secure_password 
-  has_many :tips
+  validates :username,  presence: true, length: { maximum: 255 }
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true, length: { maximum: 255 },
+                    format: { with: VALID_EMAIL_REGEX }
+  validates :school_relationship, inclusion: { in: %w(student graduate not_affiliated)}
 end
