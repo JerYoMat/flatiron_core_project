acf6acc0e479a3ea80847d9009902af4935265d8
JerYOMat <jeremy.emata@gmail.com>
2019-02-16 05:56:11 -0500

    updated strong params and form partial with password and password confirmation

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -69,6 +69,6 @@ class UsersController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def user_params
-      params.require(:user).permit(:username, :email, :password_digest, :school_relationship)
+      params.require(:user).permit(:username, :email, :password, :password_confirmation, :school_relationship)
     end
 end
