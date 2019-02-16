06f6ec65e60696409bf290acef6ae45bf6dfc838
JerYOMat <jeremy.emata@gmail.com>
2019-02-15 20:38:18 -0500

    added title helper and included in app layout

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+    def full_title(page_title = '')                  
+        base_title = "learnHELP" 
+        if page_title.empty?                             
+          base_title                                     
+        else
+          page_title + " | " + base_title                
+        end
+      end
 end
