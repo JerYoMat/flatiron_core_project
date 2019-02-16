7d72e84aba671d2e7606cd2e22b49e5927146e47
JerYOMat <jeremy.emata@gmail.com>
2019-02-15 20:11:42 -0500

    updated app to yield title from template, added title test to controller tests

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -3,16 +3,19 @@ require 'test_helper'
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   test "should get home" do
     get root_path
+    assert_select "title", "Home | learnHELP"
     assert_response :success
   end
 
   test "should get code_of_conduct" do
     get static_pages_code_of_conduct_url
+    assert_select "title", "Code of Conduct | learnHELP"
     assert_response :success
   end
 
   test "should get credits" do
     get static_pages_credits_url
+    assert_select "title", "Credits | learnHELP"
     assert_response :success
   end
 
