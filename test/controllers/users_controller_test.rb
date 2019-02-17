ef007d13014e4e13c3cb3d0aa2e50eb060604020
JerYOMat <jeremy.emata@gmail.com>
2019-02-17 18:25:20 -0500

    removed auto generated tests and added new user to tests to avoid uniqueness validation error

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -2,8 +2,15 @@ require 'test_helper'
 
 class UsersControllerTest < ActionDispatch::IntegrationTest
   setup do
-    @user = users(:one)
+    @new_user = User.new(username: "model test user", email: "model@test.com", password: "password", password_confirmation: "password", school_relationship: "student")
+    @user = users(:fixture_user_one)
+    @user.password = 'password'
+    @user.password_confirmation = 'password'
   end
+
+  teardown do 
+    Rails.cache.clear
+  end 
 
   test "should get index" do
     get users_url
@@ -17,7 +24,7 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
 
   test "should create user" do
     assert_difference('User.count') do
-      post users_url, params: { user: { email: @user.email, password_digest: @user.password_digest, school_relationship: @user.school_relationship, username: @user.username } }
+      post users_url, params: { user: { email: @new_user.email, password: @new_user.password, school_relationship: @new_user.school_relationship, username: @new_user.username } }
     end
 
     assert_redirected_to user_url(User.last)
@@ -34,7 +41,7 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
   end
 
   test "should update user" do
-    patch user_url(@user), params: { user: { email: @user.email, password_digest: @user.password_digest, school_relationship: @user.school_relationship, username: @user.username } }
+    patch user_url(@user), params: { user: { email: @user.email, password: 'password', password_confirmation: 'password', school_relationship: @user.school_relationship, username: @user.username } }
     assert_redirected_to user_url(@user)
   end
 
