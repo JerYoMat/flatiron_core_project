6cf2c92c6c942fd4e7611eba60e604f19c292218
JerYOMat <jeremy.emata@gmail.com>
2019-02-17 18:57:47 -0500

    added username being downcased to allow for index lookup to work

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,10 @@
 class User < ApplicationRecord
   has_many :tips 
   has_many :ratings 
-  before_save { email.downcase! }
 
+  
+  before_save { email.downcase! }
+  before_save { username.downcase! }
   has_secure_password 
   validates :username,  presence: true, length: { maximum: 255 }, uniqueness: {case_sensitive: false}
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
@@ -10,6 +12,7 @@ class User < ApplicationRecord
                     format: { with: VALID_EMAIL_REGEX }, uniqueness: {case_sensitive: false}
   validates :school_relationship, inclusion: { in: %w(student graduate not_affiliated)}
   validates :password, presence: true, length: {minimum: 7}
+  
   def User.digest(string)
     cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
                                                   BCrypt::Engine.cost
