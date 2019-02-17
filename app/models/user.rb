6b4c2260a2698ac5d47622bff9617b195f9d1f56
JerYOMat <jeremy.emata@gmail.com>
2019-02-17 18:27:22 -0500

    added digest method which uses lower Bcrypt cost in dev env

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,4 +10,10 @@ class User < ApplicationRecord
                     format: { with: VALID_EMAIL_REGEX }, uniqueness: {case_sensitive: false}
   validates :school_relationship, inclusion: { in: %w(student graduate not_affiliated)}
   validates :password, presence: true, length: {minimum: 7}
+  def User.digest(string)
+    cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
+                                                  BCrypt::Engine.cost
+    BCrypt::Password.create(string, cost: cost)
+  end
+
 end
