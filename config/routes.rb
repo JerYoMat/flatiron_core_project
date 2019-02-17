29f235ea3e20c602903227b081ac604748cc3973
JerYOMat <jeremy.emata@gmail.com>
2019-02-17 18:52:58 -0500

    added necessary routes for tips, units, lessons and ratings

--- config/routes.rb
+++ config/routes.rb
@@ -3,10 +3,15 @@ Rails.application.routes.draw do
   get    '/login',   to: 'sessions#new'
   post   '/login',   to: 'sessions#create'
   delete '/logout',  to: 'sessions#destroy'
-  resources :users
   get '/signup', to: 'users#new'
   post '/signup', to: 'users#create'
   get 'static_pages/code_of_conduct', to: 'static_pages#code_of_conduct'
   get 'static_pages/credits', to: 'static_pages#credits'
+  resources :users
+  resources :tips 
+  resources :units, only: [:index, :show]
+  resources :lessons, only: [:show] 
+  resources :ratings, only: [:new, :edit, :update, :destroy] 
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
