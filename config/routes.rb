7c0087d4b4804dc825e5210902ad033a51cf6830
JerYOMat <jeremy.emata@gmail.com>
2019-02-17 15:58:55 -0500

    moved around resources to have root at top

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,6 @@ Rails.application.routes.draw do
   resources :users
   get '/signup', to: 'users#new'
   post '/signup', to: 'users#create'
- 
   get 'static_pages/code_of_conduct', to: 'static_pages#code_of_conduct'
   get 'static_pages/credits', to: 'static_pages#credits'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
