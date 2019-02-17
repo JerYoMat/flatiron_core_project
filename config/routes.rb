da9dbefc7de1530f786b45c537b86c8dcad90b67
JerYOMat <jeremy.emata@gmail.com>
2019-02-17 15:41:33 -0500

    updated routes for sessions

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,12 @@
 Rails.application.routes.draw do
-  get 'sessions/new'
-  get 'sessions/create'
-  get 'sessions/destroy'
+  root 'static_pages#home'
+  get    '/login',   to: 'sessions#new'
+  post   '/login',   to: 'sessions#create'
+  delete '/logout',  to: 'sessions#destroy'
   resources :users
   get '/signup', to: 'users#new'
   post '/signup', to: 'users#create'
-  root 'static_pages#home'
+ 
   get 'static_pages/code_of_conduct', to: 'static_pages#code_of_conduct'
   get 'static_pages/credits', to: 'static_pages#credits'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
