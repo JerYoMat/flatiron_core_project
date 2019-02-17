b36c4592e018d43ce71cc19bcc50daa4fb806501
JerYOMat <jeremy.emata@gmail.com>
2019-02-17 15:58:37 -0500

    updated create route to log in user, added helper methods to sessionshelper and included in applicationhelper, added form to log in to hopefully take either username or password

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,10 +1,11 @@
 module ApplicationHelper
-    def full_title(page_title = '')                  
-        base_title = "learnHELP" 
-        if page_title.empty?                             
-          base_title                                     
-        else
-          page_title + " | " + base_title                
-        end
-      end
+  include SessionsHelper  
+  def full_title(page_title = '')                  
+    base_title = "learnHELP" 
+    if page_title.empty?                             
+      base_title                                     
+    else
+      page_title + " | " + base_title                
+    end
+  end
 end
