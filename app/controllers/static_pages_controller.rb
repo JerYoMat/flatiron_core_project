6fc17d444886632ee3d0391b06a3c334030fccdd
JerYOMat <jeremy.emata@gmail.com>
2019-02-19 20:39:09 -0500

    nixed slider from static pages controller, removed scripts, put back carousel code, removed auto-generated text from show and deleted temp storage

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,12 +1,8 @@
 class StaticPagesController < ApplicationController
   def home
-    @units = Unit.all
+   @units = Unit.all
   end
 
-  def slider
-    @units = Unit.all
-    render json: @units, status: 200
-  end 
   def code_of_conduct
   end
 
