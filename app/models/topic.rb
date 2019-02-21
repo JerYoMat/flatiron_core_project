b53f441e2aceee82949639b1772b15362811288f
JerYOMat <jeremy.emata@gmail.com>
2019-02-20 20:37:30 -0500

    updated association

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -1,7 +1,6 @@
 class Topic < ApplicationRecord
     validates :unit_id, presence: true 
-    belongs_to :unit 
-    has_many :ratings 
+    belongs_to :unit  
     has_many :topic_tips 
     has_many :tips, through: :topic_tips
 end
