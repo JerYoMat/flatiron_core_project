d5144b48c9a8428096834aae876a190b9a2998ef
JerYOMat <jeremy.emata@gmail.com>
2019-02-20 20:35:01 -0500

    updated join table to be topic_tips not lesson_tips

--- db/migrate/20190218000052_create_lesson_tips.rb
+++
@@ -1,9 +0,0 @@
-class CreateLessonTips < ActiveRecord::Migration[5.2]
-  def change
-    create_table :lesson_tips do |t|
-      t.belongs_to :lesson 
-      t.belongs_to :tip 
-      t.timestamps
-    end
-  end
-end
