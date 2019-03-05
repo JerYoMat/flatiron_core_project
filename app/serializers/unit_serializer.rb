37f60f496874eb5622daee7365e6df46f8cb812d
JerYOMat <jeremy.emata@gmail.com>
2019-03-04 23:27:18 -0500

    removed all routes with exception of root, signup flow and login flow

--- app/serializers/unit_serializer.rb
+++ app/serializers/unit_serializer.rb
@@ -1,3 +1,5 @@
 class UnitSerializer < ActiveModel::Serializer
-  attributes :id, :title, :image_path
+  attributes :id, :title, :image_path, :course_order 
 end
+
+
--- config/routes.rb
+++ config/routes.rb
@@ -5,15 +5,5 @@ Rails.application.routes.draw do
   delete '/logout',  to: 'sessions#destroy'
   get '/signup', to: 'users#new'
   post '/signup', to: 'users#create'
-  get 'static_pages/code_of_conduct', to: 'static_pages#code_of_conduct'
-  get 'static_pages/acknowledgements', to: 'static_pages#acknowledgements'
-  get 'static_pages/about', to: 'static_pages#about'
-  get 'topics/:id/tips', to: 'topics#topic_tips'
-  resources :users
-  resources :tips 
-  resources :units, only: [:index, :show]
-  resources :lessons, only: [:show] 
-  resources :ratings, only: [:new, :edit, :update, :destroy] 
-
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
