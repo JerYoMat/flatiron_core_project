8e57e0a654a8af484d9ebbb81d2000104748e49b
JerYOMat <jeremy.emata@gmail.com>
2019-03-14 13:36:21 -0400

    updated sessions controller with separate action for remote login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,9 +2,19 @@ class SessionsController < ApplicationController
   def new
   end
 
+
+  def remote_login 
+    @user = User.find_by(email: params[:email].downcase)
+    if @user && @user.authenticate(params[:password])
+      log_in @user
+      render json: @user, status: 201 
+    else
+      render json: {'response': 'invalid creds'} 
+    end 
+  end 
+ 
   def create
     @user = User.find_by(email: params[:session][:email_or_username].downcase) || User.find_by(username: params[:session][:email_or_username].downcase) 
-   
     if @user && @user.authenticate(params[:session][:password])
       log_in @user
       redirect_to @user
@@ -13,7 +23,7 @@ class SessionsController < ApplicationController
       render 'new'
     end
   end
-
+ 
   def destroy
     log_out if logged_in?
     redirect_to root_url
